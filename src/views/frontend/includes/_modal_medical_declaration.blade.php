3948c663918651ff4e41d73d1e3892a218ae6c66
Karel Brijs <karel@chuck.be>
2021-10-18 15:51:50 +0000

    bs5 fix modal button

--- src/views/frontend/includes/_modal_medical_declaration.blade.php
+++ src/views/frontend/includes/_modal_medical_declaration.blade.php
@@ -3,9 +3,7 @@
         <div class="modal-content">
 	      	<div class="modal-header clearfix text-left test-start">
 		        <h5 class="modal-title">Medische Verklaring</h5>
-		        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-		          	<span aria-hidden="true">&times;</span>
-		        </button>
+		        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 	      	</div>
 	      	<div class="modal-body">
 		        <div class="row">
